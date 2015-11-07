11e4836416f1b0d455f989cc47b0f9e3b146080f
PierreRAFFA <pierre.raffa@gmail.com>
2015-11-07 13:10:43 +0000

    Indentation + eslint configuration

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 'use strict';
-angular.module('test', [
-  // load your modules here
-  'main', // starting with the main module
+angular.module('wildwordwest', [
+    // load your modules here
+    'main', // starting with the main module
 ]);
--- app/main/controllers/debug.controller.js
+++ app/main/controllers/debug.controller.js
@@ -1,29 +1,29 @@
 'use strict';
 angular.module('main')
-.controller('DebugCtrl', function ($log, Main, Config) {
+    .controller('DebugCtrl', function ($log, Main, Config) {
 
-  $log.log('Hello from your Controller: DebugCtrl in module main:. This is your controller:', this);
+        $log.log('Hello from your Controller: DebugCtrl in module main:. This is your controller:', this);
 
-  // bind data from services
-  this.someData = Main.someData;
-  this.ENV = Config.ENV;
-  this.BUILD = Config.BUILD;
+        // bind data from services
+        this.someData = Main.someData;
+        this.ENV = Config.ENV;
+        this.BUILD = Config.BUILD;
 
-  // PASSWORD EXAMPLE
-  this.password = {
-    input: '', // by user
-    strength: ''
-  };
-  this.grade = function () {
-    var size = this.password.input.length;
-    if (size > 8) {
-      this.password.strength = 'strong';
-    } else if (size > 3) {
-      this.password.strength = 'medium';
-    } else {
-      this.password.strength = 'weak';
-    }
-  };
-  this.grade();
+        // PASSWORD EXAMPLE
+        this.password = {
+            input: '', // by user
+            strength: ''
+        };
+        this.grade = function () {
+            var size = this.password.input.length;
+            if (size > 8) {
+                this.password.strength = 'strong';
+            } else if (size > 3) {
+                this.password.strength = 'medium';
+            } else {
+                this.password.strength = 'weak';
+            }
+        };
+        this.grade();
 
-});
+    });
